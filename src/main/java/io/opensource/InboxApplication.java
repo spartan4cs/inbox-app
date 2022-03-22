3cd265dc74c767ecee7c72cea252f70fabe542f6
spartan4cs <aakashkumbhare@gmail.com>
2022-03-23 01:43:55 +0530

    cassandra db connection

--- src/main/java/io/opensource/InboxApplication.java
+++ src/main/java/io/opensource/InboxApplication.java
@@ -1,7 +1,11 @@
 package io.opensource;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +24,16 @@ public class InboxApplication {
 		System.out.println(principal);
 		return principal.getAttribute("name");
 	}
-	
+
+	/**
+	 * this is necessary to have boot app use astra secure bundle to connect to
+	 * cassandra db
+	 */
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
 
 }
